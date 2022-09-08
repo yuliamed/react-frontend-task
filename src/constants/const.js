1a98398a9a14a6d0644ca2f32af2b789b37c3d3f
yulia medvedskaya <yuliamed@list.ru>
2022-09-08 12:04:23 +0300

    roll back from deploying

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,5 +1,5 @@
-export const URL = 'alb-for-ecs-back-217100900.eu-west-2.elb.amazonaws.com/api/v1';
-
+//export const URL = 'alb-for-ecs-back-217100900.eu-west-2.elb.amazonaws.com/api/v1';
+//export const URL = '';
 export const BASE_USER_PICTURE = "https://img.crm-onebox.com//media/35/7b/357bc5f62a78f388e2808c0f8c6eb0c3.png";
 
 export const CANCEL_ORDER_STATUS = "CANCELED"
