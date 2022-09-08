b1bf88271365845f0ca66667dc32ba0d0eee65c1
yulia medvedskaya <yuliamed@list.ru>
2022-09-08 17:26:01 +0300

    change header for unauthorized user

--- src/Components/common/headers/PublicHeaderContainer.js
+++ src/Components/common/headers/PublicHeaderContainer.js
@@ -1,12 +1,26 @@
 import { NavLink } from "react-router-dom";
-import { Layout, Space } from "antd";
-const {Header, Footer, Content } = Layout;
+import { Layout, Row, Col, } from "antd";
+
+const { Header, } = Layout;
 const UserHeaderContainer = () => {
+
     return (<Layout>
-        <Header align="center">
-            <NavLink to="/sign-in">SignIn</NavLink>
-            <NavLink to="/sign-up">SignUp</NavLink>
-            {/* <NavLink to="/logout" onClick={(event) => { event.preventDefault(); dispatch(logout()) }}>LogOut</NavLink> */}
+        <Header>
+            <Row>
+                <Col flex="1 1 200px"><NavLink to="/sign-up">SignUp</NavLink></Col>
+                <Col flex="0 1 300px">
+                    <Row justify="end">
+                        <Col span={8}
+                        >
+                            <NavLink to="/sign-in">SignIn</NavLink>
+                        </Col>
+                        <Col
+                        >
+                            <NavLink to="/sign-up">SignUp</NavLink>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </Header></Layout>
     )
 }
