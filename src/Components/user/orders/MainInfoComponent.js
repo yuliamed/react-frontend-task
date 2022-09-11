d6435d3f2cda0ae69bb2be44f6c8c98f093525f4
yulia medvedskaya <yuliamed@list.ru>
2022-09-11 19:56:25 +0300

    add enam for statuses

--- src/Components/user/orders/MainInfoComponent.js
+++ src/Components/user/orders/MainInfoComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import { Card, Tag } from 'antd';
+import { ORDER_STATUSES } from '../../../constants/const';
+
 class MainInfoComponent extends Component {
     constructor(props) {
         super(props);
@@ -8,14 +10,23 @@ class MainInfoComponent extends Component {
             status: props.status,
             autoPicker: props.autoPicker,
         };
-        //thisObj = this;
+        this.getTagColor = this.getTagColor.bind(this);
+    }
+    getTagColor(statusName) {
+        switch (statusName) {
+            case ORDER_STATUSES.CANCELED: return "red";
+            case ORDER_STATUSES.CLOSED: return "blue";
+            case ORDER_STATUSES.CREATED: return "gold";
+            case ORDER_STATUSES.IN_PROCESS: return "cyan";
+        }
     }
     render() {
         return (
             <Card title="Main data" size="small">
                 <p><b>Date of order: </b>{this.state.creationDate.substr(0, 10)} </p>
-                <p><b>Status of order: </b>{this.state.status.name}</p>
-                <p><b>Auto-picker: </b> {this.state.autoPicker == null ? "does not set yet" : this.state.autoPicker.name}</p>
+                <p><b>Status of order: </b><Tag color={this.getTagColor(this.state.status.name)}>{this.state.status.name}</Tag > </p>
+                <p><b>Auto-picker: </b> {this.state.autoPicker == null ? <Tag color="magenta">does not set yet </Tag>: 
+                <Tag >this.state.autoPicker.name</Tag>}</p>
             </Card>
         );
     }
--- src/constants/const.js
+++ src/constants/const.js
@@ -3,3 +3,9 @@
 export const BASE_USER_PICTURE = "https://img.crm-onebox.com//media/35/7b/357bc5f62a78f388e2808c0f8c6eb0c3.png";
 
 export const CANCEL_ORDER_STATUS = "CANCELED"
+export const ORDER_STATUSES = {
+    CREATED: "CREATED",
+    IN_PROCESS: "IN_PROCESS",
+    CLOSED: "CLOSED",
+    CANCELED: "CANCELED"
+};
